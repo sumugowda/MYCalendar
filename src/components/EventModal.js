d2198661d7633431263fd0f5b275dfe219a62331
Sumanth Gowda <122675059+sumugowda@users.noreply.github.com>
2024-04-07 19:07:38 +0530

    fixed issue labels issue

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -1,6 +1,10 @@
 import React, { useContext, useState } from "react";
 import GlobalContext from "../context/GlobalContex";
 const labelClases = ["indigo", "gray", "red", "green", "blue", "purple"];
+// eslint-disable-next-line
+ const labeltClases = ["bg-indigo-200", "bg-gray-200", "bg-red-200", "bg-green-200", "bg-blue-200", "bg-purple-200",
+ "text-indigo-200", "text-gray-200", "text-red-200", "text-green-200", "text-blue-200", "text-purple-200",
+ "accent-indigo-200", "accent-gray-200", "accent-red-200", "accent-green-200", "accent-blue-200", "accent-purple-200"]; // due to tailwind error
 
 function EventModal() {
   const { setShowEventModel, daySelected, dispatchCalEvent, selectedEvent } =
@@ -34,9 +38,9 @@ function EventModal() {
 
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
-      <form className="bg-white rounded-lg shadow-2xl w-1/4">
-        <header className="bg-amber-100 px-4 py-2 flex justify-between items-center rounded-lg">
-          <span className="material-symbols-outlined text-gray-700 text-5xl font-bold">
+      <form className="bg-zinc-900 rounded-lg shadow-2xl w-1/4">
+        <header className="bg-zinc-900 px-4 py-2 flex justify-between items-center rounded-lg">
+          <span className="material-symbols-outlined text-gray-500 text-5xl font-bold">
             drag_handle
           </span>
           <div>
@@ -44,14 +48,14 @@ function EventModal() {
                 <span onClick={()=> {
                     dispatchCalEvent({type:"delete", payload:selectedEvent })
                     setShowEventModel(false)
-                    }} className="material-symbols-outlined text-gray-700 text-5xl font-bold cursor-pointer">
+                    }} className="material-symbols-outlined text-gray-500 text-5xl font-bold cursor-pointer">
                 delete
               </span>
             )}
           <button onClick={() => {
               setShowEventModel(false);
                }}>
-            <span className="material-symbols-outlined text-gray-700 text-5xl font-bold">
+            <span className="material-symbols-outlined text-gray-500 text-5xl font-bold">
               Close
             </span>
           </button>
@@ -67,11 +71,11 @@ function EventModal() {
               value={title}
               onChange={(e) => setTitle(e.target.value)}
               required
-              className="pt-3  text-gray-700 text-xl font-semibold pb-2 w-full border-gray-400 ease-out duration-100 hover:border-b-2 focus:outline-none "
+              className="pt-3 bg-zinc-900  text-gray-300 text-xl font-semibold pb-2 w-full border-gray-300 ease-out duration-100 hover:border-b-2 focus:outline-none "
             />
-            <span className="material-symbols-outlined text-4xl">schedule</span>
-            <p> {daySelected.format("dddd, MMMM DD")} </p>
-            <span className="material-symbols-outlined text-4xl">segment</span>
+            <span className="material-symbols-outlined text-4xl text-gray-500">schedule</span>
+            <p className="text-gray-400"> {daySelected.format("dddd, MMMM DD")} </p>
+            <span className="material-symbols-outlined text-4xl text-gray-500">segment</span>
             <input
               type="text"
               name="description"
@@ -79,23 +83,23 @@ function EventModal() {
               value={description}
               onChange={(e) => setDescription(e.target.value)}
               required
-              className="pt-3  text-gray-700 pb-2 w-full ease-out duration-100 hover:border-b-2 focus:outline-none focus:border-b-slate-950 "
+              className="pt-3 bg-zinc-900  text-gray-300 pb-2 w-full ease-out duration-100 hover:border-b-2 focus:outline-none focus:border-b-slate-950 "
             />
-            <span className="material-symbols-outlined text-4xl">
+            <span className="material-symbols-outlined text-4xl text-gray-500">
               bookmark_border
             </span>
             <div className="flex gap-x-2">
               {labelClases.map((lablClases, i) => (
                 <span
                   key={i}
-                  className={`bg-${lablClases}-400 w-8 h-8 rounded-full flex item-center justify-center cursor-pointer `}
+                  className={`bg-${lablClases}-200 w-8 h-8 rounded-full flex  justify-center cursor-pointer `}
                   onClick={() => {
                     setSelectedlabel(lablClases);
                   }}
                 >
                   {selectedlabel === lablClases && (
-                    <span className="material-symbols-outlined text-white  font-bold text-xl">
-                      check
+                    <span className="material-symbols-outlined text-gray-900 font-extrabold text-3xl">
+                      task_alt
                     </span>
                   )}
                 </span>
@@ -103,7 +107,7 @@ function EventModal() {
             </div>
           </div>
         </div>
-        <footer className="flex justify-end border-t p-3 mt-5">
+        <footer className="flex justify-end  p-3 mt-5">
           <button
             type="submit"
             onClick={handleSubmit}
--- src/components/Labels.js
+++ src/components/Labels.js
@@ -7,9 +7,10 @@ function Labels() {
     <React.Fragment>
         <p className='text-white font-bold mt-10 '>Labels</p>
         {labels.map(({label: lbl,checked}, idx)=> (
+          
             <label key={idx} className=' items-center mt-3 block cursor-pointer'>
                 <input type="checkbox" checked={checked} onChange={() => updateLabel({label: lbl, checked : !checked})} className={`h-5 w-5 accent-${lbl}-200 rounded focus:ring-0 cursor-pointer `} />
-                <span className='  ml-2 text-white '>{lbl}</span>
+                <span className={`ml-2 text-${lbl}-200`} >{lbl.toUpperCase()}</span>
             </label>
         ))}
     </React.Fragment>
